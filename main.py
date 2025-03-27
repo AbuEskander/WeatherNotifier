68ae8cbc1dfcd4ea55259c6df30f9e70eaec8bb2
Proton <basselawni1@gmail.com>
2025-03-27 05:05:54 +0300

    Added Comments for you to understand the chaos I'm writing

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
         API_KEY = os.getenv("API_KEY")  # Replace with your valid API key from OpenWeatherMap
         res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={API_KEY}&units=metric")
         print(res.json())
-        with open("Weather.json","w") as file:
+        with open("Weather.json","w") as file: # These 2 lines are just for debuging and visualizing the Json file, I don't know a better way yet :P
                 json.dump(res.json(),file,indent=4)
         temp = res.json()["main"]["temp"]
         print(temp)
