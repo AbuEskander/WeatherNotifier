f9b74003c0a9a29b5d2c2d98934011631092b2c6
Proton <basselawni1@gmail.com>
2025-03-27 06:28:21 +0300

    Lol it's done, First experiance with Python Automation task, and it was fun

--- main.py
+++ main.py
@@ -7,29 +7,32 @@ import requests
 import json
 import os
 
+
 def get_current_location()-> tuple[int, int]:
         current_location = geocoder.ip("me")
         if current_location.latlng:
                 lat,long = current_location.latlng
         return lat,long
 
+def tele_message(message:str):
+        TELE_KEY = os.getenv("TELE_KEY")
+        url = f'https://api.telegram.org/bot{TELE_KEY}/sendMEssage?chat_id={os.getenv("CHAT_ID")}&text={message}'
+        res = requests.post(url)
+        print(res.json())
 
-
-def main():
-        load_dotenv()
-        
+def main():        
         lat, long = get_current_location()
         API_KEY = os.getenv("API_KEY")  # Replace with your valid API key from OpenWeatherMap
         res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={API_KEY}&units=metric")
-        print(res.json())
         with open("Weather.json","w") as file: # These 2 lines are just for debuging and visualizing the Json file, I don't know a better way yet :P
                 json.dump(res.json(),file,indent=4)
-        temp = res.json()["main"]["temp"]
-        print(temp)
-
-
+        main = res.json()["main"]
+        weather = res.json()["weather"]
+        tele_message(f"Hey there, the temp is {main["temp"]}°C, but feels like {main["feels_like"]}°C")
+        tele_message(f"The sky is {weather[0]["description"]}")
 
 
 
 if __name__ == "__main__":
+        load_dotenv()
         main()
